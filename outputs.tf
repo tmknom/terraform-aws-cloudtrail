d73c7a3f248f1ecc7dc6aedc1987c25540a435e9
tmknom <tmk.nom@gmail.com>
2018-09-17 17:57:28 +0900

    Implement cloudtrail

---
+++ outputs.tf
@@ -0,0 +1,18 @@
+# Output Configuration
+#
+# https://www.terraform.io/docs/configuration/outputs.html
+
+output "cloudtrail_arn" {
+  value       = "${aws_cloudtrail.default.arn}"
+  description = "The Amazon Resource Name of the trail."
+}
+
+output "cloudtrail_name" {
+  value       = "${aws_cloudtrail.default.id}"
+  description = "The name of the trail."
+}
+
+output "cloudtrail_home_region" {
+  value       = "${aws_cloudtrail.default.home_region}"
+  description = "The region in which the trail was created."
+}
