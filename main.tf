3597264e31d23ddcf84b6171bd94ec2f232e4af8
tmknom <tmk.nom@gmail.com>
2018-10-26 23:49:42 +0900

    Add comment

--- main.tf
+++ main.tf
@@ -7,8 +7,23 @@ resource "aws_cloudtrail" "default" {
   name           = "${var.name}"
   s3_bucket_name = "${var.s3_bucket_name}"
 
-  enable_logging                = "${var.enable_logging}"
-  is_multi_region_trail         = "${var.is_multi_region_trail}"
+  # When you create a trail, logging is turned on automatically. You can turn off logging for a trail.
+  # https://docs.aws.amazon.com/awscloudtrail/latest/userguide/cloudtrail-turning-off-logging.html
+  enable_logging = "${var.enable_logging}"
+
+  # When you create a trail that applies to all regions,
+  # CloudTrail records events in each region and delivers the CloudTrail event log files to an S3 bucket that you specify.
+  # https://docs.aws.amazon.com/awscloudtrail/latest/userguide/how-cloudtrail-works.html
+  is_multi_region_trail = "${var.is_multi_region_trail}"
+
+  # For global services such as IAM, STS, CloudFront, and Route 53,
+  # events are delivered to any trail that includes global services,
+  # and are logged as occurring in US East (N. Virginia) Region.
+  # https://docs.aws.amazon.com/awscloudtrail/latest/userguide/cloudtrail-concepts.html#cloudtrail-concepts-global-service-events
   include_global_service_events = "${var.include_global_service_events}"
-  enable_log_file_validation    = "${var.enable_log_file_validation}"
+
+  # To determine whether a log file was modified, deleted, or unchanged after CloudTrail delivered it,
+  # you can use CloudTrail log file integrity validation.
+  # https://docs.aws.amazon.com/awscloudtrail/latest/userguide/cloudtrail-log-file-validation-intro.html
+  enable_log_file_validation = "${var.enable_log_file_validation}"
 }
